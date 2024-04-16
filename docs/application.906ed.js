a22d67cdc9e7ec72552b31861f0e6accda92e600
buma <bumaya1024@163.com>
2024-04-16 16:59:05 +0800

    update

--- docs/application.906ed.js
+++ docs/application.906ed.js
@@ -16,7 +16,7 @@ System.register([], function (_export, _context) {
         function Application() {
           _classCallCheck(this, Application);
 
-          this.settingsPath = 'src/settings.fb87d.json';
+          this.settingsPath = 'src/settings.12efd.json';
           this.showFPS = false;
         }
 
